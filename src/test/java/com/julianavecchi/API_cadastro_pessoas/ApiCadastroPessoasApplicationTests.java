51ebd233c5cd67936c7c735b0af408591a928f7f
Juliana <julianaaparecidavecchi@gmail.com>
2026-02-16 17:14:22 -0300

    First commit

---
+++ src/test/java/com/julianavecchi/API_cadastro_pessoas/ApiCadastroPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianavecchi.API_cadastro_pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
