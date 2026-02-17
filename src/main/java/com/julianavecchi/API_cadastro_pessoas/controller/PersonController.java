ea12b5d028082ea00bc2803a5c74ef32edc3d237
Juliana <julianaaparecidavecchi@gmail.com>
2026-02-17 15:40:54 -0300

    feat: create entity Person

--- src/main/java/com/julianavecchi/API_cadastro_pessoas/controller/PersonController.java
+++ src/main/java/com/julianavecchi/API_cadastro_pessoas/controller/PersonController.java
@@ -1,4 +1,4 @@
-package com.julianavecchi.API_cadastro_pessoas;
+package com.julianavecchi.API_cadastro_pessoas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class PersonController {
 
     @GetMapping("/HelloWorld")
     public String HellowWorld(){
---
+++ src/main/java/com/julianavecchi/API_cadastro_pessoas/model/PersonModel.java
@@ -0,0 +1,49 @@
+package com.julianavecchi.API_cadastro_pessoas.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_person")
+public class PersonModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public PersonModel() {
+    }
+
+    public PersonModel(String name, String email, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
