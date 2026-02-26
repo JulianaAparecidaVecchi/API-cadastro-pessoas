b92d8e74e3414b0aaefebdaf2679fe7bae943edc
Juliana <julianaaparecidavecchi@gmail.com>
2026-02-26 13:54:47 -0300

    chore: add DTO and manual mapping between Model and DTO

--- src/main/java/com/julianavecchi/peoplemanagementapi/dto/PersonDTO.java
+++ src/main/java/com/julianavecchi/peoplemanagementapi/dto/PersonDTO.java
@@ -1,4 +1,22 @@
 package com.julianavecchi.peoplemanagementapi.dto;
 
+import com.julianavecchi.peoplemanagementapi.model.GameModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PersonDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime birthDate;
+    private String motherName;
+    private GameModel favoriteGame;
+
+
 }
