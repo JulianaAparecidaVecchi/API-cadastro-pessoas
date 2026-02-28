60757ea99844c64d39e4d5e3dff9884c94d12cd9
Juliana <julianaaparecidavecchi@gmail.com>
2026-02-28 13:45:23 -0300

    refactor: add annotation @ToString to models

--- src/main/java/com/julianavecchi/peoplemanagementapi/model/PersonModel.java
+++ src/main/java/com/julianavecchi/peoplemanagementapi/model/PersonModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ToString(exclude = "favorite_game_id")
 public class PersonModel {
 
     @Id
